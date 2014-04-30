1877d20b987cba200d99d2f4cbd055b34a40b120
chris-wood <christopherwood07@gmail.com>
2014-04-29 20:33:38 -0700

    Unit unit unit... test.

--- src/test/ConsumerTest.c
+++ src/test/ConsumerTest.c
@@ -1,11 +1,15 @@
-#include "../AnonConsumer.h"
+#include "../UpstreamProxy.h"
 #include "../Proxy.h"
 #include "../Crypto.h"
 #include "../CryptoWrapper.h"
+#include "../Util.h"
 
 int main(int argc, char** argv)
 {
-	struct ccn_charbuf* orig = NULL;
+	struct ccn_charbuf* orig = ccn_charbuf_create();
+	ccn_name_init(orig);
+	ccn_name_append_str(orig, "rawr");
+	printf("name = %s\n", ccn_charbuf_as_string(orig));
 
 	// TODO	
 
