c37ebf6fd1263d3a160279cb60dc76314507f39c
chris-wood <christopherwood07@gmail.com>
2014-05-08 11:07:45 -0700

    New data && notes

---
+++ gen.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import random
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 2)) + ","),
+print("")
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 2)) + ","),
+print("")
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 2)) + ","),
+print("")
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 3)) + ","),
+print("")
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 3)) + ","),
+print("")
+
+for i in range(int(sys.argv[1])):
+	print(str(random.random() * 10 + random.randint(0, 3)) + ","),
+print("")
